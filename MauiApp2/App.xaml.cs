559a5c3001d17d1334499579c9802d27b205c53b
Banurega <banuregakannan@gmail.com>
2022-12-29 15:47:22 +0800

    Add project files.

---
+++ MauiApp2/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiApp2;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+        SecureStorage.SetAsync("sample", "sample");
+        var sample = SecureStorage.GetAsync("sample").Result;
+    }
+}
